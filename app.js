50aeec1eaff3573771d0400afa4eb98d37edbb99
techieatkct <108172869+techieatkct@users.noreply.github.com>
2022-09-04 20:13:18 +0530

    Initial commit

--- app.js
+++ app.js
@@ -32,9 +32,7 @@ function validateGuess(guess){
             displayMessage(`Game Over! Number was ${randomNumber}`);
             endGame();
         } else {
-        //Display previous guessed numbers
         displayGuesses(guess);
-        //Check guess and display if wrong
         checkGuess(guess);
         }
     }
@@ -81,4 +79,22 @@ function newGame(){
         startOver.removeChild(p);
         playGame = true;
     })
+}
+
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
 }
